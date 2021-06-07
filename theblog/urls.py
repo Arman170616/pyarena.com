32adc0cbb0c17106618780ce089e6e07c42afdb7
pyarena <Arman170616>
2021-06-07 09:41:51 +0600

    deletePostView added

--- theblog/urls.py
+++ theblog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import AddPostView, ArticleDetailView, HomeView, UpdatePostView
+from .views import AddPostView, ArticleDetailView, HomeView, UpdatePostView, DeletePostView
 
 urlpatterns = [
 
@@ -8,6 +8,7 @@ urlpatterns = [
     path('article/<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
     path('add_post/', AddPostView.as_view(), name='add_post'),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
+    path('article/<int:pk>/remove', DeletePostView.as_view(), name='delete_post'),
 
 
 ]
--- theblog/views.py
+++ theblog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 #from theblog.forms import PostForm
 from .forms import PostForm, EditForm
+
+from django.urls import reverse_lazy
 # Create your views here.
 
 
@@ -14,6 +16,7 @@ class HomeView(ListView):
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
+    ordering = ['-id']
 
 
 class AddPostView(CreateView):
@@ -29,3 +32,9 @@ class UpdatePostView(UpdateView):
     form_class = EditForm
     template_name = 'update_post.html'
     #fields = ['title', 'body']
+
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
