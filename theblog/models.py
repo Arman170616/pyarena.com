c6e63699a3549ea32ecf9e3d23cff3977a14f04e
pyarena <Arman170616>
2021-06-07 10:02:22 +0600

    blog ordering added

--- theblog/models.py
+++ theblog/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 from ckeditor.fields import RichTextField
 from django.urls import reverse
-
+from datetime import datetime, date
 # Create your models here.
 
 
@@ -12,9 +12,11 @@ class Post(models.Model):
     title_tag = models.CharField(max_length=255, default="pyarenablogs")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = RichTextField(blank=True, null=True)
+    post_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
 
     def get_absolute_url(self):
-        return reverse('article-detail', args=[str(self.id)])
+        # return reverse('article-detail', args=[str(self.id)])
+        return reverse('home')
--- theblog/views.py
+++ theblog/views.py
@@ -16,7 +16,7 @@ class HomeView(ListView):
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
-    ordering = ['-id']
+    #ordering = ['-id']
 
 
 class AddPostView(CreateView):
